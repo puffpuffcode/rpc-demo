f70ae3219f53375eda1ffb03e10a4ef2ca5e77a5
shiotya <shiotya@hotmail.com>
2023-02-26 16:08:30 +0800

    🍭 add tcp version.

---
+++ go-rpc/tcp/server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net"
+	"net/rpc"
+)
+
+type Args struct {
+	X,Y int
+}
+
+type ServiceA struct {}
+
+func (s *ServiceA) Serve(args *Args, reply *int) error {
+	*reply = args.X + args.Y
+	return nil
+}
+
+func main(){
+	service := new(ServiceA)
+	// register a service
+	rpc.Register(service)
+	// based on tcp protocol
+	l, _ := net.Listen("tcp", ":19090")
+	// serve
+	for {
+		c, _ := l.Accept()
+		rpc.ServeConn(c)
+	}
+}
